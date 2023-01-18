8f0cf04e7c7bf01dd4e367963c32e89f025b6055
Cristhian Moyolema <57960807+crizzgualoto@users.noreply.github.com>
2023-01-17 21:54:44 -0500

    Agregar archivos de proyecto.

---
+++ ClaseMagistralSemana6/Registro.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClaseMagistralSemana6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            try { 
+            WebClient client = new WebClient();
+            var parametros = new System.Collections.Specialized.NameValueCollection();
+            parametros.Add("codigo", txtId.Text);
+            parametros.Add("nombre", txtNombre.Text);
+            parametros.Add("apellido", txtApellido.Text);
+            parametros.Add("edad", txtEdad.Text);
+            client.UploadValues("http://192.168.22.6/Servicios/estudiante.php", "POST", parametros);
+                DisplayAlert("MENSAJE", "Se registro Ok " + txtNombre.Text+ " " + txtApellido.Text, "Cerrar");
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("ALERTA", ex.Message, "Cerrar");
+            }
+        }
+
+        private void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
